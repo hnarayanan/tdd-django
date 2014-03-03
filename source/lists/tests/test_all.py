26b0e550bb189e0686677bb2006c3a13614d5763
Harish Narayanan <hnarayanan@gmail.com>
2014-03-03 14:09:55 +0100

    Move unit tests to a separate subfolder

--- source/lists/tests/test_all.py
+++ source/lists/tests/test_all.py
@@ -3,8 +3,8 @@ from django.http import HttpRequest
 from django.core.urlresolvers import resolve
 from django.template.loader import render_to_string
 
-from .models import Item, List
-from .views import home_page
+from lists.models import Item, List
+from lists.views import home_page
 
 
 class HomePageTest(TestCase):
